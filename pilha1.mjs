457c10f5fccb438133c4cf9d8c32c4d55e3c63b9
XandyGomes <x3.computadores@hotmail.com>
2022-05-12 09:08:14 -0300

    Aula

--- pilha1.mjs
+++ pilha1.mjs
@@ -1,3 +1,15 @@
+/*
+    PILHA
+    É uma estrutura de dados em que, usando uma lista linear (vetor) como
+    base, permite inserções apenas no final e remoções também a partir do
+    final.
+    Com isso, temos o comportamento conhecido como LIFO = Last In, First Out
+    (último a entrar, primeiro a sair).
+    Pilhas são usadas em tarefas computacionais que requerem a inversão da
+    ordem de entrada dos dados.
+*/
+
+
 //PALÍNDROMO: TEXTO QUE É LIDO DE FORMA INVERSA.
 
 let frase = 'Socorram-me, subi no ônibus em Marrocos';
